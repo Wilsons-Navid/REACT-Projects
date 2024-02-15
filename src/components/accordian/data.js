2a61e843db1d758ab493912505a052a7b9068a69
Wilsons-Navid <wadotiwawil@gmail.com>
2024-02-15 20:20:45 +0200

    Accordion

---
+++ src/components/accordian/data.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+      id: 1,
+      question: 'What is React?',
+      answer: 'React is a JavaScript library for building user interfaces.',
+    },
+    {
+      id: 2,
+      question: 'What is JSX?',
+      answer: 'JSX is a syntax extension for JavaScript used in React to describe the structure of UI components.',
+    },
+    {
+      id: 3,
+      question: 'What are React components?',
+      answer: 'React components are reusable building blocks that encapsulate logic and UI elements.',
+    },
+    {
+      id: 4,
+      question: 'What is state in React?',
+      answer: 'State is an object that stores data and determines how the component renders and behaves.',
+    },
+    {
+      id: 5,
+      question: 'What is a React hook?',
+      answer: 'Hooks are functions that allow you to use React features in functional components.',
+    },
+
+  ];
+
+  export default data;
