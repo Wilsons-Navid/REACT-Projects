62d6778822700031cc55db3618c9992026143dbf
Wilsons-Navid <wadotiwawil@gmail.com>
2024-02-16 19:17:40 +0200

    gen random

---
+++ generate_random_color/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import RandomColor from "./components";
+
+function App() {
+  return (
+    <>
+      <RandomColor />
+    </>
+  );
+}
+
+export default App;
